76829b20bc5a5116458e53ec0b51938603db3a1a
Roman <popov.roman.e@gmail.com>
2024-08-08 19:43:41 +0500

    started project template

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,8 @@ import React from "react"
 import { createRoot } from "react-dom/client"
 import { Provider } from "react-redux"
 import App from "./App"
-import "./index.css"
 import { store } from "./redux/index"
+import "./globals.scss"
 
 const container = document.getElementById("root")
 
